3e97ab142a91dd2e1ba66ff41f3cf996f490518f
Peter Kwok <peter.kwok94@gmail.com>
2023-07-13 19:22:52 -0400

    finished logic and added styling

--- Markdown Previewer Js/app/js/script.js
+++ Markdown Previewer Js/app/js/script.js
@@ -1,3 +1,7 @@
+marked.use({
+    breaks: true
+})
+
 const editor = document.querySelector('#editor')
 const preview = document.querySelector('#preview')
 
@@ -7,7 +11,7 @@ function convert() {
 }
 
 const text =
-    '# Here is a sample heading\n\nHere is a sample [link](https://github.com/PeterKwok1)\n\n## Here is a sample sub heading\n\nHere is sample code, `<div><div>`\n\nHere is a sample code block,\n```\nfunction sampleFunc(param1, param2) {\n    return param1 + param2\n}\n```\n\nHere is a sample list,\n- first item\n- second item\n- third item\n\nHere is a sample blockquote,\n> Sample blockquote\n\nHere is an image,\n\n![Cat](https://wallpaperaccess.com/full/235613.jpg)'
+    '# Here is a sample heading\n\nHere is a sample [link](https://github.com/PeterKwok1)\n\n## Here is a sample sub heading\n\nHere is sample code, `<div><div>`\n\nHere is a sample code block,\n```\nfunction sampleFunc(param1, param2) {\n    return param1 + param2\n}\n```\n\nHere is a sample unordered list,\n- first item\n- second item\n- third item\n\nHere is a sample ordered list,\n1. first item\n2. second item\n3. third item\n\nHere is a sample table,\n\nHeader 1|Header 2|Header 3\n---|---|---\ncontent|content|content\ncontent|content|content\n\nHere is a sample blockquote,\n> Sample blockquote\n\nHere is an image,\n\n![Cat](https://wallpaperaccess.com/full/235613.jpg)\n\nHere is **bolded text**.\n\nHere is _italic text_.\n\nHere is ~~crossed out text~~.'
 editor.textContent = text
 convert()
 
