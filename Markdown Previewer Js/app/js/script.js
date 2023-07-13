6c2b56cc1985a345e4748e85c9dc99f5b04f01e2
Peter Kwok <peter.kwok94@gmail.com>
2023-07-13 01:32:44 -0400

    added more markdown preview

--- Markdown Previewer Js/app/js/script.js
+++ Markdown Previewer Js/app/js/script.js
@@ -7,7 +7,7 @@ function convert() {
 }
 
 const text =
-    '# Here is a sample heading\n\nHere is a sample [link](https://github.com/PeterKwok1)\n\n## Here is a sample sub heading\n\nHere is sample code, `<div><div>`\n\nHere is a sample code block,\n```\nfunction sampleFunc(param1, param2) {\n    return param1 + param2\n}\n```'
+    '# Here is a sample heading\n\nHere is a sample [link](https://github.com/PeterKwok1)\n\n## Here is a sample sub heading\n\nHere is sample code, `<div><div>`\n\nHere is a sample code block,\n```\nfunction sampleFunc(param1, param2) {\n    return param1 + param2\n}\n```\n\nHere is a sample list,\n- first item\n- second item\n- third item\n\nHere is a sample blockquote,\n> Sample blockquote\n\nHere is an image,\n\n![Cat](https://wallpaperaccess.com/full/235613.jpg)'
 editor.textContent = text
 convert()
 
