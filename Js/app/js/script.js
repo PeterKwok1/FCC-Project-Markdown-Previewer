2dfdce5a70eaf132cbfab159987bcf1bf3db6fb7
Peter Kwok <peter.kwok94@gmail.com>
2023-08-22 17:09:09 -0400

    rename

--- Js/app/js/script.js
+++ Js/app/js/script.js
@@ -3,7 +3,6 @@ marked.use({
 })
 
 // converter
-
 const editor = document.querySelector('#editor')
 const previewer = document.querySelector('#previewer')
 
@@ -15,14 +14,12 @@ function convert() {
 editor.addEventListener('input', convert)
 
 // initial conversion
-
 const text =
     '# Here is a sample heading\n\nHere is a sample [link](https://github.com/PeterKwok1)\n\n## Here is a sample sub heading\n\nHere is sample code, `<div><div>`\n\nHere is a sample code block,\n```\nfunction sampleFunc(param1, param2) {\n    return param1 + param2\n}\n```\n\nHere is a sample unordered list,\n- first item\n    - indented item\n- second item\n- third item\n\nHere is a sample ordered list,\n1. first item\n    1. indented item\n2. second item\n3. third item\n\nHere is a sample table,\n\nHeader 1|Header 2|Header 3\n---|---|---\ncontent|content|content\ncontent|content|content\n\nHere is a sample blockquote,\n> Sample blockquote\n\nHere is an image,\n\n![Cat](https://wallpaperaccess.com/full/235613.jpg)\n\nHere is **bolded text**.\n\nHere is _italic text_.\n\nHere is ~~crossed out text~~.'
 editor.textContent = text
 convert()
 
 // toggle expand
-
 const editorDisplayToggle = document.querySelector('#editorDisplayToggle')
 const previewerDisplayToggle = document.querySelector('#previewerDisplayToggle')
 
